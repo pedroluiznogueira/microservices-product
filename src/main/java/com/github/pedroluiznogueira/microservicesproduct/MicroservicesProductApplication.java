98086d88dd85b59ca115e8479f4ea6cf33499966
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-22 20:55:47 -0300

    first commit

---
+++ src/main/java/com/github/pedroluiznogueira/microservicesproduct/MicroservicesProductApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedroluiznogueira.microservicesproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProductApplication.class, args);
+	}
+
+}
