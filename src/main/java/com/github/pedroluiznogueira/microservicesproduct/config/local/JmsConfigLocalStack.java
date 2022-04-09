0fa13babd20d05c2f88bf9186f393eb4871eeb58
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-09 17:41:42 -0300

    chore

---
+++ src/main/java/com/github/pedroluiznogueira/microservicesproduct/config/local/JmsConfigLocalStack.java
@@ -0,0 +1,57 @@
+package com.github.pedroluiznogueira.microservicesproduct.config.local;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.destination.DynamicDestinationResolver;
+
+import javax.jms.Session;
+
+@Configuration
+@EnableJms
+@Profile("local")
+public class JmsConfigLocalStack {
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    // sqs connection
+    private SQSConnectionFactory sqsConnectionFactory;
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        // region and credentials to access queue
+        sqsConnectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClient.builder()
+                        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", Regions.US_EAST_1.getName()))
+                        .withCredentials(new DefaultAWSCredentialsProviderChain())
+                        .build());
+
+        // jms default config, threads per/ queue, acknowledge that message was proccessed
+        DefaultJmsListenerContainerFactory factory =
+                new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(sqsConnectionFactory);
+        factory.setDestinationResolver(new DynamicDestinationResolver());
+        factory.setConcurrency("2");
+        factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+
+        return factory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        return new JmsTemplate(sqsConnectionFactory);
+    }
+}
